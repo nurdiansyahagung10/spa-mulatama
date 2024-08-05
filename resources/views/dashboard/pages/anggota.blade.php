d6e52f4b552e5b1c6791d91f2b9a7f6bd8ce0047
nurdiansyahagung10 <nurdiansyahagung10@gmail.com>
2024-08-05 15:30:29 +0700

    repair table data

--- resources/views/dashboard/pages/anggota.blade.php
+++ resources/views/dashboard/pages/anggota.blade.php
@@ -22,7 +22,7 @@
     </div>
 
     <div style="overflow:auto" class="p-10 text-sm w-full h-auto mb-10 border-b-0 rounded-t-2xl min-h-[70vh] border backdrop-blur-sm">
-        <table class="table table-fixed text-center align-middle">
+        <table class="table table-fixed text-sm text-center align-middle">
             <thead class=" dark:text-white">
                 <tr class="dark:border-stone-400">
                     <th class="font-medium text-black dark:text-white ">No</th>
