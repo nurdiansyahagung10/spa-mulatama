81ef7d7544bf89e5dee5705044c60a5ae74e5478
nurdiansyahagung10 <nurdiansyahagung10@gmail.com>
2024-08-05 11:29:46 +0700

    add delete user

--- resources/views/dashboard/pages/users.blade.php
+++ resources/views/dashboard/pages/users.blade.php
@@ -69,7 +69,7 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                                     </svg></div>
-                            <form method="post" action="{{ url ('staff')}}/${item.id}/delete" tabindex="0" class="dropdown-content menu rounded-box z-[1] w-24 mt-2 dark:border dark:bg-base-300 bg-black text-stone-300 border-0  dark:border-stone-400 p-2 shadow">
+                            <form method="post" action="staff/${item.id}/delete" tabindex="0" class="dropdown-content menu rounded-box z-[1] w-24 mt-2 dark:border dark:bg-base-300 bg-black text-stone-300 border-0  dark:border-stone-400 p-2 shadow">
                                 @method('delete')   
                                 @csrf
                                 <li><a href="{{ url('staff') }}/${item.id}/edit" class="hover:text-white ">Edit</a></li>
