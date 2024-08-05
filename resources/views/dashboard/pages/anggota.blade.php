7c34030390f5771b5e062bba1071d7a9839de4ba
nurdiansyahagung10 <nurdiansyahagung10@gmail.com>
2024-08-05 23:12:07 +0700

    add edit anggota

--- resources/views/dashboard/pages/anggota.blade.php
+++ resources/views/dashboard/pages/anggota.blade.php
@@ -21,8 +21,8 @@
         </div>
     </div>
 
-    <div style="overflow:auto" class="p-10 w-full h-auto mb-10 border-b-0 rounded-t-2xl min-h-[70vh] border backdrop-blur-sm">
-        <table class="table table-fixed  text-center align-middle">
+    <div class="p-10 overflow-auto w-full h-auto mb-10 border-b-0 rounded-t-2xl min-h-[70vh] border backdrop-blur-sm">
+        <table class="table  text-center align-middle">
             <thead class="text-sm dark:text-white">
                 <tr class="dark:border-stone-400">
                     <th class="font-medium text-black dark:text-white ">No</th>
