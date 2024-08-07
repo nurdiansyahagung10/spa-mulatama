c4b5be1a8f2684778c72c78ce05c2a3a62f1180e
nurdiansyahagung10 <nurdiansyahagung10@gmail.com>
2024-08-07 13:27:50 +0700

    repair delete anggota

--- resources/views/layout/notif.blade.php
+++ resources/views/layout/notif.blade.php
@@ -10,9 +10,9 @@
 @endif
 @if(session('success'))
 <div style="white-space: normal" class="  toast p-[0.6rem] z-40 toast-end">
-  <div class="border text-left text-black alert gap-2 justify-between flex border-stone-200 dark:bg-transparent dark:text-white bg-white/20 backdrop-blur-lg notif{{$loop->iteration}}">
+  <div class="border text-left text-black alert gap-2 justify-between flex border-stone-200 dark:bg-transparent dark:text-white bg-white/20 backdrop-blur-lg notif">
     <span class="border-r pr-3">{{session('success')}}</span>
-    <button class="px-3" onclick="hidden_notif('notif{{$loop->iteration}}')">X</button>
+    <button class="px-3" onclick="hidden_notif('notif')">X</button>
   </div>
 </div>
 
