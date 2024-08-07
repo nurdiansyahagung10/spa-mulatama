5405c1d77c4afe0adb3c4eb73eb099fda2c6b83d
nurdiansyahagung10 <nurdiansyahagung10@gmail.com>
2024-08-07 13:35:04 +0700

    repair notif

--- resources/views/layout/notifsuccess.blade.php
+++ resources/views/layout/notifsuccess.blade.php
@@ -1,13 +1,4 @@
-@if ($errors->any())
-<div style="white-space: normal" class="  toast p-[0.6rem] z-40 toast-end">
-        @foreach ($errors->all() as $error)
-        <div class="border text-left text-black alert gap-2 justify-between flex border-stone-200 dark:bg-transparent dark:text-white bg-white/20 backdrop-blur-lg notif{{$loop->iteration}}">
-          <span class="border-r pr-3">{{$error}}</span>
-          <button class="px-3" onclick="hidden_notif('notif{{$loop->iteration}}')">X</button>
-        </div>
-        @endforeach
-      </div>
-@endif
+
 @if(session('success'))
 <div style="white-space: normal" class="  toast p-[0.6rem] z-40 toast-end">
   <div class="border text-left text-black alert gap-2 justify-between flex border-stone-200 dark:bg-transparent dark:text-white bg-white/20 backdrop-blur-lg notif">
