6433d429e22b745722b2e12bd6dbdc2ee6007057
nurdiansyahagung10 <nurdiansyahagung10@gmail.com>
2024-08-07 12:56:59 +0700

    repair notif

--- resources/views/dashboard/pages/edituser.blade.php
+++ resources/views/dashboard/pages/edituser.blade.php
@@ -13,7 +13,7 @@
         @foreach ($errors->all() as $error)
         <div class="border text-left text-black alert gap-2 justify-between flex border-stone-200 dark:bg-transparent dark:text-white bg-white/20 backdrop-blur-lg notif{{$loop->iteration}}">
           <span class="border-r pr-3">{{$error}}</span>
-          <button class="px-3" onclick="hidden_notif('{{$loop->iteration}}')">X</button>
+          <button class="px-3" onclick="hidden_notif('notif{{$loop->iteration}}')">X</button>
         </div>
         @endforeach
       </div>
