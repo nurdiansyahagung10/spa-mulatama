5f973cad21ad975033188ca7783eb7dce39f85b8
nurdiansyahagung10 <nurdiansyahagung10@gmail.com>
2024-08-20 13:29:05 +0700

    repair anggota

--- resources/views/dashboard/pages/storting.blade.php
+++ resources/views/dashboard/pages/storting.blade.php
@@ -24,7 +24,7 @@
     </div>
 
     <div style="overflow: auto"
-        class="p-10 w-full h-auto mb-10 border-b-0 rounded-t-2xl min-h-[70vh] border backdrop-blur-sm">
+        class="p-10 w-full h-auto mb-10 border-b-0 rounded-t-2xl min-h-[70vh] max-h-[70vh] border backdrop-blur-sm">
         <table class="table  text-center align-middle">
             <thead class="text-sm dark:text-white">
                 <tr class="dark:border-stone-400">
