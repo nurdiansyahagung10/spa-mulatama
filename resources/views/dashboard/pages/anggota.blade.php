99226d1f24b3058d3af44a9c29a9e1e8ed2f89c2
nurdiansyahagung10 <nurdiansyahagung10@gmail.com>
2024-08-26 03:09:51 +0700

    another repair

--- resources/views/dashboard/pages/anggota.blade.php
+++ resources/views/dashboard/pages/anggota.blade.php
@@ -322,10 +322,6 @@ listtablebodyanggota.innerHTML += `
                            `;
                        });
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 6b045260027799aacba08732ce4310762278a505
                        
                         console.log(btnsaerchbyvalue.trim());
                         if (btnsaerchbyvalue.trim() == "Nama") {
