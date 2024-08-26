05b8a452376add8d9cbc951f036cbaf73f952c52
nurdiansyahagung385 <nurdiansyahagung385@gmail.com>
2024-08-26 06:48:56 +0000

    repair commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JsonController;
+use App\Http\Middleware\HaveNtAuth;
+use App\Http\Middleware\AuthAdmin;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('anggota/list/get', [JsonController::class, 'anggota'])->name('anggotaget');
+Route::get('pdl/list/get', [JsonController::class, 'pdl'])->name('pdlget');
+Route::get('user/list/get', [JsonController::class, 'user'])->name('userget');
+Route::get('dropping/list/get', [JsonController::class, 'dropping'])->name('droppingget');
+Route::get('cabang/list/get', [JsonController::class, 'cabang'])->name('cabangget');
+Route::get('storting/list/get', [JsonController::class, 'storting'])->name('stortingget');
