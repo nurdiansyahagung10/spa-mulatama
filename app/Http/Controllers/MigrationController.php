1046c1fb63cae68b150c942d24c75bdf8bbdbc54
nurdiansyahagung385 <nurdiansyahagung385@gmail.com>
2024-09-08 15:46:44 +0000

    recover data

--- app/Http/Controllers/MigrationController.php
+++ app/Http/Controllers/MigrationController.php
@@ -14,10 +14,10 @@ class MigrationController extends Controller
             Artisan::call('migrate:fresh');
 
             // Jika sukses, kembali ke halaman dengan pesan sukses
-            return back()->with('success', 'Migration fresh berhasil dijalankan.');
+            return return response()->json('berhasil', 200);
         } catch (\Exception $e) {
             // Jika ada error, kembalikan dengan pesan error
-            return back()->with('error', 'Terjadi kesalahan: ' . $e->getMessage());
+            return return response()->json($e->getMessage(), 200);
         }
     }
 }
